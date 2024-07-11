5da178da5b2514651df122d5ef3d61aa7346bf94
RamezCh <ramiz_chreide@outlook.com>
2024-07-11 19:11:10 +0300

    Selection of Header Elements

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+// Selection of the elements
+// Header elements
+const balanceEl = document.getElementById('balance');
+const incomeEl = document.getElementById('income');
+const expenseEl = document.getElementById('expense');
