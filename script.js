5147ae232c79ae8c7558b1077d017765b0064dcf
RamezCh <ramiz_chreide@outlook.com>
2024-07-11 20:41:36 +0300

    Fix Sign Error

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ let expense = 0;
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
-  signDisplay: 'always',
+  signDisplay: 'never',
 });
 
 // Functions
