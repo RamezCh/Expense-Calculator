f8d050a8d79050b915af2236d0b7f130086625ae
RamezCh <ramiz_chreide@outlook.com>
2024-07-11 19:24:19 +0300

    Selecting Done

--- script.js
+++ script.js
@@ -5,3 +5,21 @@
 const balanceEl = document.getElementById('balance');
 const incomeEl = document.getElementById('income');
 const expenseEl = document.getElementById('expense');
+// Add Transaction Elements
+const form = document.getElementById('transactionForm');
+// Display Transaction Elements
+const list = document.getElementById('transactionList');
+const status = document.getElementById('status');
+// Transaction History Elements
+let expenses = [];
+let income = [];
+let balance = 0;
+
+// Currency Formatter
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  signDisplay: 'always',
+});
+
+// Checkbox logic
